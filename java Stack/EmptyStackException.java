2e3b849aabaa3ae010e12906edccd7da06c66b2a
rony31416 <ronymajumder1171@gmail.com>
2023-04-21 13:32:06 +0600

    java stack

---
+++ java Stack/EmptyStackException.java
@@ -0,0 +1,15 @@
+package generics;
+
+public class EmptyStackException extends RuntimeException {
+	public EmptyStackException() {
+
+		this("Stack is empty");
+	}
+
+	public EmptyStackException(String exception)
+
+	{
+		super(exception);
+
+	}
+}
---
+++ java Stack/FullStackException.java
@@ -0,0 +1,13 @@
+package generics;
+
+public class FullStackException extends RuntimeException {
+
+	public FullStackException() {
+
+		this("Stack is full");
+	}
+
+	public FullStackException(String exception) {
+		super(exception);
+	}
+}
